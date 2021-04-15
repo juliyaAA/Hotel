b704004aee3ab5dc36e67b36f9b63a2ac781f390
Yulya <yulya919@yandex.ru>
2021-04-16 01:19:33 +0300

    media

--- src/js/script.js
+++ src/js/script.js
@@ -7,24 +7,27 @@ $(document).ready(function () {
         navText: ["<img src='src/icons/prev.png'>", ""],
         responsive: {
             0: {
-                items: 1
-            },
-            400: {
                 items: 2
             },
-            700: {
+            550: {
                 items: 3
             },
+            740: {
+                items: 4
+            }, 
+            950: {
+                items: 5
+            }, 
             1100: {
                 items: 4
-            },
-            1200: {
+            }, 
+            1300: {
                 items: 5
             }
         }
     });
     $(".slide-two").owlCarousel({
-        items: 5,
+        items: 4,
         dots: false,
         nav: true,
         loop: true,
@@ -34,32 +37,34 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
-            400: {
+            685: {
                 items: 2
+
             },
-            600: {
+            1005: {
                 items: 3
             },
-            1000: {
+            1500: {
                 items: 4
             },
-            1200: {
+            1650: {
                 items: 5
             }
+
         }
     });
 
     // gallery
     var sync1 = $("#sync1");
     var sync2 = $("#sync2");
-    var slidesPerPage = 4; 
+    var slidesPerPage = 4;
     var syncedSecondary = true;
 
     sync1.owlCarousel({
         items: 1,
         slideSpeed: 2000,
         nav: true,
-        autoplay: false, 
+        autoplay: false,
         dots: false,
         loop: true,
         responsiveRefreshRate: 200,
@@ -67,7 +72,7 @@ $(document).ready(function () {
     }).on('changed.owl.carousel', syncPosition);
 
     sync2
-        .on('initialized.owl.carousel', function() {
+        .on('initialized.owl.carousel', function () {
             sync2.find(".owl-item").eq(0).addClass("current");
         })
         .owlCarousel({
@@ -77,7 +82,7 @@ $(document).ready(function () {
             margin: 10,
             smartSpeed: 200,
             slideSpeed: 500,
-            slideBy: slidesPerPage, 
+            slideBy: slidesPerPage,
             responsiveRefreshRate: 100
         }).on('changed.owl.carousel', syncPosition2);
 
@@ -91,7 +96,7 @@ $(document).ready(function () {
         if (current > count) {
             current = 0;
         }
-        
+
         sync2
             .find(".owl-item")
             .removeClass("current")
@@ -116,15 +121,15 @@ $(document).ready(function () {
         }
     }
 
-    sync2.on("click", ".owl-item", function(e) {
+    sync2.on("click", ".owl-item", function (e) {
         e.preventDefault();
         var number = $(this).index();
         sync1.data('owl.carousel').to(number, 300, true);
     });
 
     // Закрытие gallery
-    jQuery(function($){
-        $(document).mouseup(function (e){ // событие клика по веб-документу
+    jQuery(function ($) {
+        $(document).mouseup(function (e) { // событие клика по веб-документу
             var div = $(".popup__slider"); // тут указываем ID элемента
             if (!div.is(e.target) // если клик был не по нашему блоку
                 && div.has(e.target).length === 0) { // и не по его дочерним элементам
@@ -132,33 +137,68 @@ $(document).ready(function () {
             }
         });
     });
+    $('.hamburger').on('click', function () {
+        $('.menu').slideToggle();
+        $(this).toggleClass('active');
+        $('.header').toggleClass('active');
+        $('.menu__item').toggleClass('active');
+        $('.menu__registr').slideToggle();
+    });
 
-});
-// Menu
-const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    $('.menu__item').on('click', function () {
+        $('.menu__item').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.promo__info_list a').on('click', function () {
+        $('.promo__info_list a').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.navigation__ul_list a').on('click', function () {
+        $('.navigation__ul_list a').removeClass('active');
+        $(this).addClass('active');
+    });
+    // arrow 
+    $('.wrapper__arrow').on('click', function () {
+        $('.wrapper__arrow').toggleClass('active');
+        $('.wrapper__flex').slideToggle();
+    });
+    $('#btn').on('click', function () {
+        $('.wrapper__flex').slideToggle();
+        $('.wrapper__arrow').removeClass('active');
+    });
+    // date
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: 'Предыдущий',
+        nextText: 'Следующий',
+        currentText: 'Сегодня',
+        monthNames: ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Не',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger_active');
-    menu.classList.toggle('menu_active');
-});
+    var date = new Date();
+    date.setDate(date.getDate() + 1);
+    var date1 = new Date();
+    date1.setDate(date1.getDate());
 
-menuItem.forEach(item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+    $("#datepicker, #datepicker-2").datepicker({
+        dateFormat: "dd.mm.yy",
+        minDate: date1
+    });
+    $("#datepicker-1, #datepicker-3").datepicker({
+        dateFormat: "dd.mm.yy",
+        minDate: date
     });
 });
-
-// find
-const find = document.querySelector('.find'),
-    aside = document.querySelector('.aside');
-
-find.addEventListener('click', () => {
-    aside.classList.toggle('aside_absolute');
-});
-
 // gallery
 const gallery = document.getElementById('gallery'),
     popup = document.getElementById('popup');
@@ -169,7 +209,7 @@ gallery.addEventListener('click', () => {
 });
 // Закрытие gallery по кнопке Esc
 window.addEventListener("keydown", (event) => {
-    if ( event.keyCode == 27 ) {
+    if (event.keyCode == 27) {
         popup.style.display = 'none';
-    } 
+    }
 });
